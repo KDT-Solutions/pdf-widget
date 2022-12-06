b0ddbd6804146f396c034dfb19ae4168ee48cc10
Kim Thurnherr <kdt@kdt-solutions.ch>
2022-12-06 22:53:17 +0100

    Update pdf-widget.php

--- widgets/pdf-widget/pdf-widget.php
+++ widgets/pdf-widget/pdf-widget.php
@@ -102,7 +102,7 @@ class Pdf_Widget extends Widget_Base {
 		$api_key = $pdf_widget__options['api_key_0'];
  ?>
  <div id="adobe-dc-view"></div>
- <script src="https://documentcloud.adobe.com/view-sdk/viewer.js"></script>
+ <script src="<?php echo esc_url(WP_PLUGIN_DIR . '/pdf-widget/widgets/pdf-widget/js/viewer.js'); ?>"></script>
  <script type="text/javascript">
  	document.addEventListener("adobe_dc_view_sdk.ready", function(){
  		var adobeDCView = new AdobeDC.View({clientId: "<?php echo wp_kses($api_key); ?>", divId: "adobe-dc-view"});
@@ -124,7 +124,7 @@ class Pdf_Widget extends Widget_Base {
 	protected function _content_template() {
 		?>
 		<div id="adobe-dc-view"></div>
-		<script src="https://documentcloud.adobe.com/view-sdk/viewer.js"></script>
+		<script src="<?php echo esc_url(WP_PLUGIN_DIR . '/pdf-widget/widgets/pdf-widget/js/viewer.js'); ?>"></script>
 		<script type="text/javascript">
 			document.addEventListener("adobe_dc_view_sdk.ready", function(){
 				var adobeDCView = new AdobeDC.View({clientId: "<?php echo wp_kses($api_key); ?>", divId: "adobe-dc-view"});
